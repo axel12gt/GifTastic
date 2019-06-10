caf1285469c9d03932e35b180d2adb816f287125
Dameon Brown <axel12gt@live.com>
2019-06-10 15:38:35 -0400

    finished default ajax call before adding gif pausing functionality

--- assets/js/code.js
+++ assets/js/code.js
@@ -5,7 +5,34 @@ $("button").on("click",function(){
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(){
+    }).then(function(response){
+      console.log(response)
+      // gathers all the data from api into an array
+      var results = response.data
+      // loops through the data and appends to the div
+      for(var i = 0; i < results.length; i++){
+        var animalDiv = $("<div>")
+        // sets variable for rating at index i of response.data
+        var rating = results[i].rating
+        // sets inner text to rating 
+        var p = $("<p>").text("rating: " + rating)
+        // appends the information to animal div
+        animalDiv.append(p)
+        //sets img tag to the animal image
+        var animalImage = $("<img>")
+        //adds src to attribute results at index i from fixed height url
+        animalImage.attr("src", results[i].images.fixed_height.url)
+        //appends animalImage to animalDiv
+        animalDiv.append(animalImage)
+        //prepends animal div to gifs div
+        $(".gifs").prepend(animalDiv)
+
+      }
+
       
+
+
+
+
     })
 })
